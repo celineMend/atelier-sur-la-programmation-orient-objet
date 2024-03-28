b180ed40fca045f1ee353309e7e9c5549a8f3c62
celineMend <celine.mendy@unchk.edu.sn>
2024-03-28 13:04:27 +0000

    creation de la classe voiture et l'initation de ces propriéte

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 class Voiture{
+    //déclaration des propriétes public
     public $marque;
     public $modele;
     public $kilometrage;
@@ -17,11 +18,50 @@ public function getMarque() {
 public function setMarque($toyota) {
     $this->marque =$toyota ;
 }
-function démarer (){
-    echo "je m'appelle $this->marque ";
+function demarrer (){
+    echo "cette voiture est de manque $this->marque et V8 comme $this->modele la voiture à un $this->kilometrage de 100 et a été créer cette $this->annee";
  }
+
+public function getModele() {
+    return $this->modele;
+}
+public function setModele(){
+    $this->modele=$v8;
+}
+public function getKilometrage(){
+    return $this->kilometrage;
+}
+public function setKilometrage($kilometrage) {
+    $this->kilometrage = $kilometrage;
+}
+Public function getAnnee(){
+    return $this->modele ;
+}
+public function setAnnee($annee) {
+    $this->annee = $annee;
+}
  
 }
+
+
+
+
+//class Voiture extends vehicule {
+   // public $couleur ;
+
+//function demarrer __construct($marque, $modele ,$kilometrage ,$annee ;$couleur) {
+    //$this->couleur = $couleur;
+    
+//}
+
+
+
+
+//}
+
+
+
 $Voiture1=new voiture("toyota", "modele","kilometrage","annee");
-$Voiture1->démarer();
+$Voiture1->demarrer();
+
 ?>
